9d5d81c7cb5d15237108077d9e85b3133c48cb7e
Josh Mallery <josh.mallery@gmail.com>
2022-08-02 10:23:17 -0600

    Successful fetch of articles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
+
+  const [articles, setArticles] = useState([])
+
+useEffect(()=>{
+
+  fetch('https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=ENPdIXAc6HGMh1AdMtzKQcdnGsYAYv0e')
+    .then(response => response.json())
+    .then(response => setArticles(response.results))
+},[])
+
   return (
     <div className="App">
       <header className="App-header">
